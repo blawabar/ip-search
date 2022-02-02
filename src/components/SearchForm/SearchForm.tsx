31fd5f3b730d07901e02c220b120fb17a547c08c
Bartosz Krolak <fake.torrent@wp.pl>
2022-02-03 00:08:46 +0100

    feat: add mocked version of SearchForm component

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,7 +1,44 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 import styles from "./SearchForm.module.scss";
+interface FormData {
+  searchPhrase: string;
+}
+
+const schema = yup
+  .object({
+    searchPhrase: yup.string().required(),
+  })
+  .required();
 
 const SearchForm = () => {
-  return <div className={styles.searchForm}>SearchForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const errorMessage = errors.searchPhrase?.message;
+  const onSubmit = (data: FormData) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.searchForm}>
+      <div className={styles.formRow}>
+        <input
+          type="search"
+          placeholder="Enter a valid IP or a domain name"
+          className={styles.searchInput}
+          {...register("searchPhrase")}
+        />
+        {errorMessage && <p className={styles.formErrMsg}>{errorMessage}</p>}
+      </div>
+      <input type="submit" value="Lookup" className={styles.submitBtn} />
+    </form>
+  );
 };
 
 export default SearchForm;
